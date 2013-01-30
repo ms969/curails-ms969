e724213637443903419016c229ca90f10e7631cd
MJ Sun <ms969@cornell.edu>
2013-01-30 19:34:02 +0000

    home, help, and about page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,54 @@
+# require 'spec_helper'
+
+# describe "StaticPages" do
+#   describe "GET /static_pages" do
+#     it "works! (now write some real specs)" do
+#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#       get static_pages_index_path
+#       response.status.should be(200)
+#     end
+#   end
+# end
+require 'spec_helper'
+
+describe "Static pages" do
+
+    describe "Home page" do
+        it "should have the content 'INFO 2310 MicroPoster'" do
+            visit '/static_pages/home'
+            page.should have_content('INFO 2310 MicroPoster')
+        end
+
+        it "should have the title 'Home'" do
+            visit '/static_pages/home'
+            page.should have_selector('title',
+                :text => "INFO2310 Microposter | Home")
+        end
+    end
+
+    describe "Help page" do
+        it "should have the h1 'Help'" do
+            visit '/static_pages/help'
+            page.should have_selector('h1', text: 'Help')
+        end
+
+        it "should have the title 'Help'" do
+            visit '/static_pages/help'
+            page.should have_selector('title',
+                :text => "INFO2310 Microposter | Help")
+        end
+    end
+
+    describe "About page" do
+        it "should have the content 'About'" do
+            visit '/static_pages/about'
+            page.should have_content("About")
+        end
+
+        it "should have the title 'About'" do
+            visit '/static_pages/about'
+            page.should have_selector('title',
+                :text => "INFO2310 Microposter | About")
+        end
+    end
+end
